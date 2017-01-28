0e0ec6a43b1c74bdd11d4804ad56bf389ee8984d
caoduythanh <caoduythanhcantho@gmail.com>
2017-01-28 14:57:24 +0700

    Add HOST_API, HOST_DENSITY_API, HOST_CAMERA_API

--- tools/env/base.ts
+++ tools/env/base.ts
@@ -1,8 +1,14 @@
 import {EnvConfig} from './env-config.interface';
 
 const BaseConfig: EnvConfig = {
-  // Sample API url
-  API: 'https://demo.com'
+	HOST_API: 'http://localhost:4000/',
+
+	HOST_DENSITY_API: 'http://localhost:4000/density/',
+
+	HOST_CAMERA_API: 'http://localhost:4000/simulation',
+
+	// Sample API url
+	API: 'https://demo.com'
 };
 
 export = BaseConfig;
