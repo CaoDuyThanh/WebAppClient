24aecb09eee04a8841d93656771c54e1f6e9bb12
caoduythanh <caoduythanhcantho@gmail.com>
2017-02-02 08:10:19 +0700

    Add HOST_QUICKSEARCH_API and HOST_STATISTIC_API

--- tools/env/base.ts
+++ tools/env/base.ts
@@ -3,9 +3,13 @@ import {EnvConfig} from './env-config.interface';
 const BaseConfig: EnvConfig = {
 	HOST_API: 'http://localhost:4000/',
 
-	HOST_DENSITY_API: 'http://localhost:4000/density/',
+	HOST_DENSITY_API: 'http://localhost:4000/density',
 
 	HOST_CAMERA_API: 'http://localhost:4000/simulation',
+
+	HOST_QUICKSEARCH_API: 'http://localhost:4000/quicksearch',
+
+	HOST_STATISTIC_API: 'http://localhost:4000/statistic',
 
 	// Sample API url
 	API: 'https://demo.com'
