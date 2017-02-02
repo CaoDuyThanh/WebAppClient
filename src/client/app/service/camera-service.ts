a456c1bfc9fb915261507db2c0b9db27820e5f2f
caoduythanh <caoduythanhcantho@gmail.com>
2017-02-02 23:37:15 +0700

    Add GetAllTrafficPoles in service

--- src/client/app/service/camera-service.ts
+++ src/client/app/service/camera-service.ts
@@ -63,6 +63,44 @@ export class CameraService{
 				});
 	}
 
+	GetAllTrafficPoles(): Observable<TrafficPole[]>{
+		let queryUrl = Config.HOST_CAMERA_API + '/alltrafficpoles';
+
+		return this.http.get(queryUrl)
+				.map((res: Response) => {
+					return (<any>res.json()).map((trafficpole: any) => {
+						return new TrafficPole({
+							PoleId: trafficpole.pole_id,
+							Lat: trafficpole.lat,
+							Lon: trafficpole.lon,
+							Width: trafficpole.width,
+							Height: trafficpole.height,
+							Name: trafficpole.name,
+							PoleAngle: trafficpole.pole_angle,
+							Type: trafficpole.type,
+							Cameras: trafficpole.cameras.map((camera: any) => {
+								return new Camera({
+									Area: camera.area,
+									OneWay: camera.one_way,
+									IsActive: camera.active,
+									Width: camera.width,
+									AngleX: camera.angle_x,
+									AngleZ: camera.angle_z,
+									Fov: camera.fov,
+									StreamId: camera.stream_id,
+									Roads: camera.road.map((latlon: any) => {
+										return new LatLon({
+											Lat: latlon.lat,
+											Lon: latlon.lon
+										});
+									})
+								});
+							})
+						});
+					});
+				});	
+	}
+
 	DeleteTrafficPole(poleId: number): Observable<any>{
 		let queryUrl = Config.HOST_CAMERA_API + '/trafficpole/' + poleId;
 
