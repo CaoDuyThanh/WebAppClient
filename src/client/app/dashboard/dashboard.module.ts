e39b7a7e9a3ca19f4668f5df5301a889ab8dc1ae
caoduythanh <caoduythanhcantho@gmail.com>
2017-02-02 08:14:33 +0700

    Add realtimestatistic component Commnet unused components

--- src/client/app/dashboard/dashboard.module.ts
+++ src/client/app/dashboard/dashboard.module.ts
@@ -7,19 +7,19 @@ import { ModalModule } from 'ng2-bootstrap/ng2-bootstrap';
 import { HomeModule } from './home/home.module';
 import { MapSettingsModule } from './map-settings/map-settings.module';
 import { CameraSettingsModule } from './camera-settings/camera-settings.module';
-import { ChartModule } from './charts/chart.module';
-import { BlankPageModule } from './blank-page/blankPage.module';
-import { TableModule } from './tables/table.module';
-import { FormModule } from './forms/forms.module';
-import { GridModule } from './grid/grid.module';
-import { BSComponentModule } from './bs-component/bsComponent.module';
-import { BSElementModule } from './bs-element/bsElement.module';
+import { RealtimeStatisticModule } from './realtime-statistic/realtime-statistic.module';
+// import { ChartModule } from './charts/chart.module';
+// import { BlankPageModule } from './blank-page/blankPage.module';
+// import { TableModule } from './tables/table.module';
+// import { FormModule } from './forms/forms.module';
+// import { GridModule } from './grid/grid.module';
+// import { BSComponentModule } from './bs-component/bsComponent.module';
+// import { BSElementModule } from './bs-element/bsElement.module';
 
 import { DashboardComponent } from './dashboard.component';
 
 import {TopNavComponent} from '../shared/index';
 import {SidebarComponent} from '../shared/index';
-
 
 @NgModule({
     imports: [
@@ -28,15 +28,21 @@ import {SidebarComponent} from '../shared/index';
     	DropdownModule,
         ModalModule,
     	HomeModule,
+
+        // Setting Modules
         MapSettingsModule,
         CameraSettingsModule,
-        ChartModule,
-        TableModule,
-        FormModule,
-        GridModule,
-    	BSComponentModule,
-        BSElementModule,
-        BlankPageModule
+
+        // Statistic Modules
+        RealtimeStatisticModule,
+        
+     //    ChartModule,
+     //    TableModule,
+     //    FormModule,
+     //    GridModule,
+    	// BSComponentModule,
+     //    BSElementModule,
+     //    BlankPageModule
     ],
     declarations: [DashboardComponent, TopNavComponent, SidebarComponent],
     exports: [DashboardComponent, TopNavComponent, SidebarComponent]
