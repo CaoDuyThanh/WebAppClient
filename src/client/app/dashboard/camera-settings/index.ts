aeec8c3cc7b66c6fbd81c22660f55e68b724a23e
caoduythanh <caoduythanhcantho@gmail.com>
2017-02-02 08:11:59 +0700

    Refactor code

--- src/client/app/dashboard/camera-settings/index.ts
+++ src/client/app/dashboard/camera-settings/index.ts
@@ -1,5 +1,5 @@
 /**
- * This barrel file provides the export for the lazy loaded ChartComponent.
+ * This barrel file provides the export for the lazy loaded CameraSettingsComponent.
  */
 export * from './camera-settings.component';
 export * from './camera-settings.route';
