087311a36b80f11bea9354622e3505a39f501506
caoduythanh <caoduythanhcantho@gmail.com>
2017-02-05 11:08:42 +0700

    Refactor code

--- src/client/app/dashboard/camera-settings/camera-settings.module.ts
+++ src/client/app/dashboard/camera-settings/camera-settings.module.ts
@@ -1,28 +1,36 @@
-import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { CameraSettingsComponent } from './camera-settings.component';
 
+// Import module
+import { PopupModule } from '../../shared/popup/index';
+
 // Import sub Component
 import { TrafficPoleEditComponent } from './sub-component/traffic-pole-edit.component';
 import { CameraEditComponent } from './sub-component/camera-edit.component';
 import { LatlonEditComponent } from './sub-component/latlon-edit.component';
+import { YesnoPanelComponent } from '../../shared/yesno-panel/yesno-panel';
+import { WarningPanelComponent } from '../../shared/warning-panel/warning-panel';
 
 @NgModule({
     imports: [
         BrowserModule,
     	RouterModule,
     	HttpModule,
+        PopupModule,
+        FormsModule
     ],
     declarations: [
     	CameraSettingsComponent,
     	TrafficPoleEditComponent,
     	CameraEditComponent,
-    	LatlonEditComponent
-
+    	LatlonEditComponent,
+        YesnoPanelComponent,
+        WarningPanelComponent
     ],
     exports: [CameraSettingsComponent]
 })
