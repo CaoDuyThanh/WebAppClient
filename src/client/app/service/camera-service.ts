68662e1a3a235ca3f85e31d685a8bb167964573a
caoduythanh <caoduythanhcantho@gmail.com>
2017-02-05 11:07:37 +0700

    Refactor code

--- src/client/app/service/camera-service.ts
+++ src/client/app/service/camera-service.ts
@@ -7,27 +7,33 @@ import { Config } from '../shared/index';
 import { TrafficPole, Camera, LatLon } from './models/CameraModel';
 
 @Injectable()
-export class CameraService{
-	constructor(private http: Http){
+export class CameraService {
+	constructor(private http: Http) {
 	}
 
-	GetNumTrafficPoles(): Observable<number>{
+	GetNumTrafficPoles(): Observable<number> {
 		let queryUrl = Config.HOST_CAMERA_API + '/numtrafficpoles/';
 
 		return this.http.get(queryUrl)
 	            .map((res: Response) => {
-	                let totalItems = res.json();
-	                return (+totalItems);
+	                let result = res.json();
+
+	                if (result.status === 'success') {
+	                	return (+result.data.num_traffic_poles);
+	                } else {
+	                	console.log(result.message);
+	                	return 0;
+	                }
 	            });
 	}
 
-	GetTrafficPoles(page: number, numItems: number): Observable<TrafficPole[]>{
+	GetTrafficPoles(page: number, numItems: number): Observable<TrafficPole[]> {
 		let params: string = [
 			`page=${page}`,
 			`num_items_per_page=${numItems}`
 		].join('&');
 		let queryUrl = Config.HOST_CAMERA_API + '/trafficpolesbypage?' + params;
-		
+
 		return this.http.get(queryUrl)
 				.map((res: Response) => {
 					return (<any>res.json()).map((trafficpole: any) => {
@@ -63,7 +69,7 @@ export class CameraService{
 				});
 	}
 
-	GetAllTrafficPoles(): Observable<TrafficPole[]>{
+	GetAllTrafficPoles(): Observable<TrafficPole[]> {
 		let queryUrl = Config.HOST_CAMERA_API + '/alltrafficpoles';
 
 		return this.http.get(queryUrl)
@@ -98,18 +104,59 @@ export class CameraService{
 							})
 						});
 					});
-				});	
+				});
 	}
 
-	DeleteTrafficPole(poleId: number): Observable<any>{
+	DeleteTrafficPole(poleId: number): Observable<any> {
 		let queryUrl = Config.HOST_CAMERA_API + '/trafficpole/' + poleId;
-
-		return this.http.delete(queryUrl)	
+		return this.http.delete(queryUrl)
 				.map((res: Response) => {
 					let result = <any>res.json();
 					return result;
 				});
 
+	}
+
+	// NOT FINISH - need add parameters to determine which camera got information
+	GetNumVehiclesCamera(camera: Camera): Observable<any> {
+		let queryUrl = Config.HOST_STATISTIC_API + '/vehicles/camera';
+		return this.http.get(queryUrl)
+            .map((res: Response) => {
+                let result = res.json();
+                if (result.status === 'success') {
+                	return result.data;
+                } else {
+                	console.log(result.message);
+                	return 0;
+                }
+            });
+	}
+
+	CreateTrafficPole(newTrafficPole: TrafficPole): Observable<any> {
+		let headers = new Headers({ 'Content-Type': 'application/json' });
+        let options = new RequestOptions({ headers: headers });
+
+		let queryUrl = Config.HOST_CAMERA_API + '/trafficpole/';
+		var dataSend = JSON.stringify(newTrafficPole.ToJSON());
+		console.log(dataSend);
+		return this.http.post(queryUrl, { data: dataSend }, options)
+		    .map((res: Response) => {
+		        let result = res.json();
+		        return result;
+		    });
+	}
+
+	EditTrafficPole(editTrafficPole: TrafficPole): Observable<any> {
+		let headers = new Headers({ 'Content-Type': 'application/json' });
+        let options = new RequestOptions({ headers: headers });
+
+		let queryUrl = Config.HOST_CAMERA_API + '/trafficpole/';
+		var dataSend = JSON.stringify(editTrafficPole.ToJSON());
+		return this.http.put(queryUrl, { data: dataSend }, options)
+		    .map((res: Response) => {
+		        let result = res.json();
+		        return result;
+		    });
 	}
 }
 
