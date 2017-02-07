5fabe7545f423947f0e13b426bf4929eb23aa373
caoduythanh <caoduythanhcantho@gmail.com>
2017-02-07 15:18:36 +0700

    add minimap component

---
+++ src/client/app/dashboard/database-statistic/sub-component/minimap.component.ts
@@ -0,0 +1,133 @@
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { ScriptHelper } from '../../../utils/script.helper';
+import { CSSHelper } from '../../../utils/css.helper';
+import { MinimapConfig } from './minimap.config';
+
+// Import services
+import { CameraService } from '../../../service/camera-service';
+
+// Import models
+import { TrafficPole, Camera } from '../../../service/models/CameraModel';
+
+declare let L: any;
+
+@Component({
+    moduleId: module.id,
+    selector: 'minimap-cmp',
+    templateUrl: 'minimap.component.html'
+})
+
+export class MinimapComponent implements OnInit {
+    private isLoadMap: boolean;
+    private trafficPoles: TrafficPole[];
+    private selectedTrafficPole: TrafficPole;
+    private mymap: any;
+
+    // Realtime camera components
+    private componentIds:number[];
+    private cameras: Camera[];
+
+    constructor(private cameraService: CameraService,
+                private elementRef: ElementRef,
+                private scriptHelper: ScriptHelper,
+                private cssHelper: CSSHelper) {
+        this.isLoadMap = false;
+        this.trafficPoles = [];
+        this.selectedTrafficPole = new TrafficPole();
+        this.componentIds = [];
+        this.cameras = [];
+    }
+
+    ngOnInit() {
+        // Load External CSS
+        var leafletCss = this.cssHelper.CreateCSSTag('stylesheet', 'text/css', '<%= CSS_SRC %>/leaflet.css');
+        this.elementRef.nativeElement.prepend(leafletCss);
+
+        // Load External Javascript
+        var leafletTag = this.scriptHelper.CreateScriptTag('text/javascript', '<%= JS_SRC %>/leaflet-src.js');
+        var leafletAjaxTag = this.scriptHelper.CreateScriptTag('text/javascript', '<%= JS_SRC %>/leaflet.ajax.min.js');
+
+        this.elementRef.nativeElement.prepend(leafletTag);
+        this.elementRef.nativeElement.prepend(leafletAjaxTag);
+    }
+
+    loadMap(): void {
+        this.mymap = L.map('minimap');
+        var rasterOption = {
+            maxZoom: MinimapConfig.MAX_ZOOM,
+            id: MinimapConfig.MAPID
+        };
+        var rasterLayer = L.tileLayer(MinimapConfig.RASTER_URL, rasterOption);
+        rasterLayer.addTo(this.mymap);
+        var rasterDisplayLayer = {
+            'Raster layer': rasterLayer
+        };
+        L.control.layers(rasterDisplayLayer).addTo(this.mymap);
+        this.mymap.setView(MinimapConfig.DEFAULT_VIEW, MinimapConfig.DEFAULT_ZOOM);
+    }
+
+    addTrafficPolesMarker(): void {
+        var iconOptions = L.icon({
+            iconUrl: '<%= JS_SRC %>/images/camera.svg',
+            iconSize:     [60, 150],
+            iconAnchor:   [30, 150],
+            popupAnchor:  [-3, -76]
+        });
+
+        this.trafficPoles.forEach((trafficPole: TrafficPole) => {
+
+            var marker = L.marker([trafficPole.Lat, trafficPole.Lon], {icon: iconOptions})
+                          .on('click', () => {
+                                this.selectedTrafficPole = trafficPole;
+
+                                var trafficPoleModal: any = $('#ShowTrafficPoleBtn');
+                                trafficPoleModal.click();
+                          });
+
+
+            marker.addTo(this.mymap);
+        });
+    }
+
+    loadCamera(): void {
+        (this.cameraService.GetAllTrafficPoles())
+            .subscribe(
+                (results: TrafficPole[]) => {
+                    this.trafficPoles = results;
+                    this.addTrafficPolesMarker();
+                },
+                (err: any) => {
+                    console.log(err);
+                }
+            );
+    }
+
+    SelectTrafficPole(trafficPole: TrafficPole): void {
+        this.mymap.panTo(L.latLng(trafficPole.Lat, trafficPole.Lon));
+    }
+
+    ClickLoadMap(): void {
+        this.isLoadMap = true;
+        this.loadMap();
+        this.loadCamera();
+    }
+
+    CreateComponentId(idx: number):string {
+        return 'Component' + idx.toString();
+    }
+
+    AddCamera(camera: Camera): void {
+        if (camera) {
+            var currentTimeStamp = Math.floor(Date.now());
+            this.componentIds.push(currentTimeStamp);
+
+            this.cameras.push(camera);
+        }
+    }
+
+    ClickDeleteCamera(componentId:number): void {
+        var idx = this.componentIds.indexOf(componentId);
+        this.componentIds.splice(idx, 1);
+        this.cameras.splice(idx, 1);
+    }
+}
---
+++ src/client/app/dashboard/database-statistic/sub-component/minimap.config.ts
@@ -0,0 +1,11 @@
+
+export class MinimapConfig {
+	static ACCESS_TOKEN: string = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw';
+	static RASTER_SRC: string = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png';
+	static RASTER_URL: string = MinimapConfig.RASTER_SRC + '?access_token=' + MinimapConfig.ACCESS_TOKEN;
+
+	static MAPID: string = 'mapbox.streets';
+	static DEFAULT_VIEW: any = [10.7688, 106.6619];
+	static DEFAULT_ZOOM: number = 16;
+	static MAX_ZOOM: number = 16;
+}
