a493b1984c112820c1391d37e45f6a61992c6a31
caoduythanh <caoduythanhcantho@gmail.com>
2017-02-07 15:20:16 +0700

    add new file

---
+++ src/client/app/dashboard/database-statistic/index.ts
@@ -0,0 +1,5 @@
+/**
+ * This barrel file provides the export for the lazy loaded RealtimeStatisticComponent.
+ */
+export * from './database-statistic.component';
+export * from './database-statistic.route';
