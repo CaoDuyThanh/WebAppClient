b575d4b22cb7b242b4e33af4d1c435965d26544e
caoduythanh <caoduythanhcantho@gmail.com>
2017-02-09 15:42:59 +0700

    Modified:
    - Change to use light version to get street density

--- src/client/app/service/density-service.ts
+++ src/client/app/service/density-service.ts
@@ -7,27 +7,32 @@ declare let dcodeIO: any;
 
 @Injectable()
 export class DensityService {
-	private densityStreetsProtobuf: any;
+	private densityStreetsLightProtobuf: any = null;
 
 	constructor(private http: Http) {
 
 	}
 
-	GetDensity(trafficPoleIds: number[]): Observable<any> {
-		var protobuf:any = dcodeIO.ProtoBuf;
-		var builder:any = protobuf.loadProtoFile('/app/service/models/streets.proto');
-		this.densityStreetsProtobuf = builder.build('DensityStreets').DensityStreets;
+	GetDensity(bounds: any, streetType: string): Observable<any> {
+		if (!this.densityStreetsLightProtobuf) {
+			var protobuf: any = dcodeIO.ProtoBuf;
+			var builder: any = protobuf.loadProtoFile('/app/service/models/streets_light.proto');
+			this.densityStreetsLightProtobuf = builder.build('DensityStreetsLight').DensityStreetsLight;
+		}
 
-		let params: string = trafficPoleIds.map((trafficPole: number) => {
-													return 'streetIds[]=' + trafficPole;
-												})
-											.join('&');
-		let queryUrl = Config.HOST_DENSITY_API + '/streetspbf?' + params;
+		let params: string = [
+			`lat_start=${bounds.lat_start}`,
+			`lat_end=${bounds.lat_end}`,
+			`lon_start=${bounds.lon_start}`,
+			`lon_end=${bounds.lon_end}`,
+			`street_type=${streetType}`
+		].join('&');
+		let queryUrl = Config.HOST_DENSITY_API + '/streetslightpbf?' + params;
 
 		return this.http.get(queryUrl, { responseType: ResponseContentType.ArrayBuffer })
 				.map((res: any) => {
 					var buffer = res.arrayBuffer();
-					var data = this.densityStreetsProtobuf.decode(buffer);
+					var data = this.densityStreetsLightProtobuf.decode(buffer);
 					return data;
 				});
 	}
