e262cf4d787e21903cb7eadea216e93e2f772a35
caoduythanh <caoduythanhcantho@gmail.com>
2017-02-20 18:04:40 +0700

    Modified:
    - Add routes for map service

--- tools/env/base.ts
+++ tools/env/base.ts
@@ -1,17 +1,19 @@
 import {EnvConfig} from './env-config.interface';
 
 const BaseConfig: EnvConfig = {
-	HOST_API: 'http://192.168.1.107:4000/',
+	HOST_API: 'http://localhost:4000/',
 
-	HOST_DENSITY_API: 'http://192.168.1.107:4000/density',
+	HOST_DENSITY_API: 'http://localhost:4000/density',
 
-	HOST_STREETS_API: 'http://192.168.1.107:4000/streets',
+	HOST_STREETS_API: 'http://localhost:4000/streets',
 
-	HOST_CAMERA_API: 'http://192.168.1.107:4000/simulation',
+	HOST_CAMERA_API: 'http://localhost:4000/simulation',
 
-	HOST_QUICKSEARCH_API: 'http://192.168.1.107:4000/quicksearch',
+	HOST_QUICKSEARCH_API: 'http://localhost:4000/quicksearch',
 
-	HOST_STATISTIC_API: 'http://192.168.1.107:4000/statistic',
+	HOST_STATISTIC_API: 'http://localhost:4000/statistic',
+
+	HOST_SERVICE_API: 'http://localhost:4000/service',
 
 	// Sample API url
 	API: 'https://demo.com'
