48786dec2aeb3d68ad8033d298034c506116d6ef
caoduythanh <caoduythanhcantho@gmail.com>
2017-03-01 15:48:27 +0700

    Modified:
    - Refactor code - group function into section

--- src/client/app/dashboard/camera-settings/camera-settings.component.ts
+++ src/client/app/dashboard/camera-settings/camera-settings.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, EventEmitter,
          ViewChild, ViewContainerRef, ReflectiveInjector, ComponentFactoryResolver
          } from '@angular/core';
-import { Pagination } from '../../utils/pagination.helper';
 import { Observer } from 'rxjs';
 
 // Import Service
@@ -18,6 +17,7 @@ import { WarningPanelComponent } from '../../shared/shared-module/warning-panel/
 
 // Import Utils
 import { EventData } from '../../utils/event.helper';
+import { Pagination } from '../../utils/pagination.helper';
 
 @Component({
     moduleId: module.id,
@@ -34,11 +34,14 @@ import { EventData } from '../../utils/event.helper';
 })
 
 export class CameraSettingsComponent implements OnInit {
+    // Constant
     public NUM_ITEMS_PER_PAGE: number = 5;
 
+    // Traffic poles data and pagination
     public TrafficPoles: TrafficPole[];
     public ChoiceTrafficPoles: boolean[];
     public Pages: Pagination;
+    private TrafficPoleInfo: string;
 
     // Create | Edit TrafficPole Modal - variables
     @ViewChild('TrafficPoleModalContainer', { read: ViewContainerRef }) private TrafficPoleModalContainer: ViewContainerRef;
@@ -54,9 +57,7 @@ export class CameraSettingsComponent implements OnInit {
 
     // Popup message
     private popupType: Observer<string>;
-    private popupMessage: string;
-
-    private TrafficPoleInfo: string;
+    private popupMessage: string;    
 
     constructor(private cameraService: CameraService,
                 private resolver: ComponentFactoryResolver) {
@@ -72,79 +73,127 @@ export class CameraSettingsComponent implements OnInit {
         this.getTrafficPoles(1);
 	}
 
-    createEmptyChoiceTrafficPole(): void {
-        this.ChoiceTrafficPoles = [];
-        for (let idx: number = 0; idx < this.TrafficPoles.length; idx++) {
-            this.ChoiceTrafficPoles.push(false);
+
+
+    // Event handling ------------------------------------------
+    createDeleteMultiItemEvent(): void {
+        if (this.YesnoModalComponent) {
+            this.YesnoModalComponent.destroy();
         }
-    }
 
-    getTrafficPoles(page: number): void {
-        (this.cameraService.GetTrafficPoles(page, this.NUM_ITEMS_PER_PAGE))
-            .subscribe(
-                (results: TrafficPole[]) => {
-                    this.TrafficPoles = results;
-                    this.createEmptyChoiceTrafficPole();
-                },
-                (err: any) => {
-                    console.log(err);
+        var response = new EventEmitter<EventData>();
+        // Create data to pass to modal
+        let data = [
+            {provide: 'Header', useValue: 'Delete selected traffic poles'},
+            {provide: 'Message', useValue: 'Selected traffic poles will be deleted. Are you sure ?'},
+            {provide: 'Response', useValue: response}
+        ];
+        let resolveData = ReflectiveInjector.resolve(data);
+        let injector = ReflectiveInjector.fromResolvedProviders(resolveData, this.YesnoModalContainer.parentInjector);
+
+        // Create component
+        let factory = this.resolver.resolveComponentFactory(YesnoPanelComponent);
+        let component = factory.create(injector);
+
+        // Add component to modal
+        this.YesnoModalContainer.insert(component.hostView);
+        this.YesnoModalComponent = component;
+
+        // Handle response
+        response.subscribe(
+            (result: string) => {
+                var closeDiv: any;
+                switch (result) {
+                    case YesnoPanelComponent.YES:
+                        let idx = 0;
+                        this.ChoiceTrafficPoles.forEach((choiceTrafficPole: boolean) => {
+                            if (choiceTrafficPole) {
+                                (this.cameraService.DeleteTrafficPole(this.TrafficPoles[idx].PoleId))
+                                    .subscribe(
+                                        (result: any) => {
+                                            if (result.status === 'success') {
+                                                this.popupMessage = 'Delete traffic pole.';
+                                                this.popupType.next(PopupComponent.Success);
+                                            } else {
+                                                this.popupMessage = 'Can not delete traffic pole.';
+                                                this.popupType.next(PopupComponent.Failure);
+                                            }
+                                        },
+                                        (err: any) => {
+                                            console.log(err);
+                                        }
+                                    );
+                            }
+                            idx++;
+                        });
+
+                        closeDiv = $('#YesnoModalBtn');
+                        closeDiv.click();
+                        let currentPage = this.Pages.GetPageActive();
+                        this.getNumItems(currentPage);
+
+                        break;
+                    case YesnoPanelComponent.NO:
+                        closeDiv = $('#YesnoModalBtn');
+                        closeDiv.click();
+                        break;
+                    default:
+                        // Do nothing here
+                        break;
                 }
-            );
+            },
+            (err: any) => {
+                console.log(err);
+            }
+        );
     }
 
-    onNumItemsChange(value: number): void {
-        this.NUM_ITEMS_PER_PAGE = value;
-        let currentPage = this.Pages.GetPageActive();
-        this.getNumItems(currentPage);
-    }
+    createWarningEvent(message: string): void {
+        if (this.YesnoModalComponent) {
+            this.YesnoModalComponent.destroy();
+        }
 
-    GetPage(page:number): void {
-        this.Pages.SetPageActive(page);
-        this.getTrafficPoles(page);
-    }
+        // Create data to pass to modal
+        var response = new EventEmitter<EventData>();
+        let data = [
+            {provide: 'Header', useValue: 'Warning'},
+            {provide: 'Message', useValue: message},
+            {provide: 'Response', useValue: response}
+        ];
+        let resolveData = ReflectiveInjector.resolve(data);
+        let injector = ReflectiveInjector.fromResolvedProviders(resolveData, this.YesnoModalContainer.parentInjector);
 
-    ClickDelete(poleId: number): void {
-        this.deleteMessage = 'Do you want to delete PoleId = ' + this.selectedTrafficPole + ' ?';
-        this.selectedTrafficPole = poleId;
-    }
+        // Create component
+        let factory = this.resolver.resolveComponentFactory(WarningPanelComponent);
+        let component = factory.create(injector);
 
-    DeleteTrafficPole(): void {
-        (this.cameraService.DeleteTrafficPole(this.selectedTrafficPole))
-            .subscribe(
-                (result: any) => {
-                    if (result.status === 'success') {
-                        this.popupMessage = 'Delete traffic pole.';
-                        this.popupType.next(PopupComponent.Success);
-                    } else {
-                        this.popupMessage = 'Can not delete traffic pole.';
-                        this.popupType.next(PopupComponent.Failure);
-                    }
+        // Add component to modal
+        this.YesnoModalContainer.insert(component.hostView);
+        this.YesnoModalComponent = component;
 
-                    let currentPage = this.Pages.GetPageActive();
-                    this.getNumItems(currentPage);
-                },
-                (err: any) => {
-                    console.log(err);
+        // Handle response
+        response.subscribe(
+            (result: string) => {
+                switch (result) {
+                    case WarningPanelComponent.CONFIRM:
+                        var closeDiv = $('#YesnoModalBtn');
+                        closeDiv.click();
+                        break;
+                    default:
+                        // Do nothing here
+                        break;
                 }
-            );
+            },
+            (err: any) => {
+                console.log(err);
+            }
+        );
     }
+    // Event handling (end) ------------------------------------
 
-    ShowTrafficPoleInfo(trafficPole: TrafficPole): void {
-        this.TrafficPoleInfo = trafficPole.ToJSON();
-    }
 
-    ClickPrevious(): void {
-        let currentPage = this.Pages.GetPageActive();
-        this.Pages.SetPageActive(currentPage - 1);
-        this.getTrafficPoles(this.Pages.GetPageActive());
-    }
 
-    ClickNext(): void {
-        let currentPage = this.Pages.GetPageActive();
-        this.Pages.SetPageActive(currentPage + 1);
-        this.getTrafficPoles(this.Pages.GetPageActive());
-    }
-
+    // Actions on table ----------------------------------------
     ClickCheckAll(element: any): void {
         for (let idx: number = 0; idx < this.ChoiceTrafficPoles.length; idx++) {
             this.ChoiceTrafficPoles[idx] = element.checked;
@@ -270,138 +319,9 @@ export class CameraSettingsComponent implements OnInit {
         );
     }
 
-    getNumItems(pageActive?: number): void {
-        (this.cameraService.GetNumTrafficPoles())
-            .subscribe(
-                (result: number) => {
-                    this.Pages = new Pagination(result, this.NUM_ITEMS_PER_PAGE);
-                    if (pageActive) {
-                        this.Pages.SetPageActive(pageActive);
-                        this.getTrafficPoles(this.Pages.GetPageActive());
-                    } else {
-                        this.Pages.SetPageActive(1);
-                        this.getTrafficPoles(this.Pages.GetPageActive());
-                    }
-                },
-                (err: any) => {
-                    console.log(err);
-                }
-            );
-    }
-
-
-    createDeleteMultiItemEvent(): void {
-        if (this.YesnoModalComponent) {
-            this.YesnoModalComponent.destroy();
-        }
-
-        var response = new EventEmitter<EventData>();
-        // Create data to pass to modal
-        let data = [
-            {provide: 'Header', useValue: 'Delete selected traffic poles'},
-            {provide: 'Message', useValue: 'Selected traffic poles will be deleted. Are you sure ?'},
-            {provide: 'Response', useValue: response}
-        ];
-        let resolveData = ReflectiveInjector.resolve(data);
-        let injector = ReflectiveInjector.fromResolvedProviders(resolveData, this.YesnoModalContainer.parentInjector);
-
-        // Create component
-        let factory = this.resolver.resolveComponentFactory(YesnoPanelComponent);
-        let component = factory.create(injector);
-
-        // Add component to modal
-        this.YesnoModalContainer.insert(component.hostView);
-        this.YesnoModalComponent = component;
-
-        // Handle response
-        response.subscribe(
-            (result: string) => {
-                var closeDiv: any;
-                switch (result) {
-                    case YesnoPanelComponent.YES:
-                        let idx = 0;
-                        this.ChoiceTrafficPoles.forEach((choiceTrafficPole: boolean) => {
-                            if (choiceTrafficPole) {
-                                (this.cameraService.DeleteTrafficPole(this.TrafficPoles[idx].PoleId))
-                                    .subscribe(
-                                        (result: any) => {
-                                            if (result.status === 'success') {
-                                                this.popupMessage = 'Delete traffic pole.';
-                                                this.popupType.next(PopupComponent.Success);
-                                            } else {
-                                                this.popupMessage = 'Can not delete traffic pole.';
-                                                this.popupType.next(PopupComponent.Failure);
-                                            }
-                                        },
-                                        (err: any) => {
-                                            console.log(err);
-                                        }
-                                    );
-                            }
-                            idx++;
-                        });
-
-                        closeDiv = $('#YesnoModalBtn');
-                        closeDiv.click();
-                        let currentPage = this.Pages.GetPageActive();
-                        this.getNumItems(currentPage);
-
-                        break;
-                    case YesnoPanelComponent.NO:
-                        closeDiv = $('#YesnoModalBtn');
-                        closeDiv.click();
-                        break;
-                    default:
-                        // Do nothing here
-                        break;
-                }
-            },
-            (err: any) => {
-                console.log(err);
-            }
-        );
-    }
-
-    createWarningEvent(message: string): void {
-        if (this.YesnoModalComponent) {
-            this.YesnoModalComponent.destroy();
-        }
-
-        // Create data to pass to modal
-        var response = new EventEmitter<EventData>();
-        let data = [
-            {provide: 'Header', useValue: 'Warning'},
-            {provide: 'Message', useValue: message},
-            {provide: 'Response', useValue: response}
-        ];
-        let resolveData = ReflectiveInjector.resolve(data);
-        let injector = ReflectiveInjector.fromResolvedProviders(resolveData, this.YesnoModalContainer.parentInjector);
-
-        // Create component
-        let factory = this.resolver.resolveComponentFactory(WarningPanelComponent);
-        let component = factory.create(injector);
-
-        // Add component to modal
-        this.YesnoModalContainer.insert(component.hostView);
-        this.YesnoModalComponent = component;
-
-        // Handle response
-        response.subscribe(
-            (result: string) => {
-                switch (result) {
-                    case WarningPanelComponent.CONFIRM:
-                        var closeDiv = $('#YesnoModalBtn');
-                        closeDiv.click();
-                        break;
-                    default:
-                        // Do nothing here
-                        break;
-                }
-            },
-            (err: any) => {
-                console.log(err);
-            }
-        );
+    ClickDelete(poleId: number): void {
+        this.deleteMessage = 'Do you want to delete PoleId = ' + this.selectedTrafficPole + ' ?';
+        this.selectedTrafficPole = poleId;
     }
 
     SelectDelete(): void {
@@ -427,4 +347,100 @@ export class CameraSettingsComponent implements OnInit {
             this.createWarningEvent('Please choose at least one traffic pole');
         }
     }
+
+    ClickPrevious(): void {
+        let currentPage = this.Pages.GetPageActive();
+        this.Pages.SetPageActive(currentPage - 1);
+        this.getTrafficPoles(this.Pages.GetPageActive());
+    }
+
+    ClickNext(): void {
+        let currentPage = this.Pages.GetPageActive();
+        this.Pages.SetPageActive(currentPage + 1);
+        this.getTrafficPoles(this.Pages.GetPageActive());
+    }
+
+    onNumItemsChange(value: number): void {
+        this.NUM_ITEMS_PER_PAGE = value;
+        let currentPage = this.Pages.GetPageActive();
+        this.getNumItems(currentPage);
+    }
+
+    GetPage(page:number): void {
+        this.Pages.SetPageActive(page);
+        this.getTrafficPoles(page);
+    }
+
+    ShowTrafficPoleInfo(trafficPole: TrafficPole): void {
+        this.TrafficPoleInfo = trafficPole.ToJSON();
+    }
+    // Actions on table (end) ----------------------------------
+
+
+
+    // Data handling -------------------------------------------
+    createEmptyChoiceTrafficPole(): void {
+        this.ChoiceTrafficPoles = [];
+        for (let idx: number = 0; idx < this.TrafficPoles.length; idx++) {
+            this.ChoiceTrafficPoles.push(false);
+        }
+    }
+    // Data handling (end) -------------------------------------
+
+
+
+    // Database interaction ------------------------------------
+    getNumItems(pageActive?: number): void {
+        (this.cameraService.GetNumTrafficPoles())
+            .subscribe(
+                (result: number) => {
+                    this.Pages = new Pagination(result, this.NUM_ITEMS_PER_PAGE);
+                    if (pageActive) {
+                        this.Pages.SetPageActive(pageActive);
+                        this.getTrafficPoles(this.Pages.GetPageActive());
+                    } else {
+                        this.Pages.SetPageActive(1);
+                        this.getTrafficPoles(this.Pages.GetPageActive());
+                    }
+                },
+                (err: any) => {
+                    console.log(err);
+                }
+            );
+    }
+
+    getTrafficPoles(page: number): void {
+        (this.cameraService.GetTrafficPoles(page, this.NUM_ITEMS_PER_PAGE))
+            .subscribe(
+                (results: TrafficPole[]) => {
+                    this.TrafficPoles = results;
+                    this.createEmptyChoiceTrafficPole();
+                },
+                (err: any) => {
+                    console.log(err);
+                }
+            );
+    }
+
+    DeleteTrafficPole(): void {
+        (this.cameraService.DeleteTrafficPole(this.selectedTrafficPole))
+            .subscribe(
+                (result: any) => {
+                    if (result.status === 'success') {
+                        this.popupMessage = 'Delete traffic pole.';
+                        this.popupType.next(PopupComponent.Success);
+                    } else {
+                        this.popupMessage = 'Can not delete traffic pole.';
+                        this.popupType.next(PopupComponent.Failure);
+                    }
+
+                    let currentPage = this.Pages.GetPageActive();
+                    this.getNumItems(currentPage);
+                },
+                (err: any) => {
+                    console.log(err);
+                }
+            );
+    }
+    // Database interaction (end) ------------------------------
 }
